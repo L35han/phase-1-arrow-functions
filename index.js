d0014d24ff1551074a12b8a6a77f19cf9d408030
Ted <ted.gitonga@student.moringaschool.com>
2024-01-17 12:31:38 +0300

    Done

---
+++ index.js
@@ -0,0 +1,27 @@
+function divide(){
+const divide=(parameter1, parameter2) => parameter1/parameter2;
+divide(2000, 100);
+}
+
+function add(){
+    const add = (a, b) => a + b;
+    }
+
+
+const sum = (parameter4, parameter5) =>{
+console.log(`Adding ${parameter4}`);
+ console.log(`Adding ${parameter5}`);
+    return parameter4 + parameter5;
+}
+sum(1,2);
+
+function divide(a, b) {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    return 2000 /100;
+  }
+
+
+const square = (a) => a * a;
+
